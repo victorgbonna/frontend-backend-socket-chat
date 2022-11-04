4a5dee3c26d9dd4f902d9be4e20ba61b1b3d6221
Victor Ogbonna <victorgbonna@gmail.com>
2022-11-04 12:20:07 +0100

    Update [recipient].jsx

--- frontendSocket/src/pages/chat/[recipient].jsx
+++ frontendSocket/src/pages/chat/[recipient].jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
-// import Chatrecipient from '../../components/chat';
 
-
+//look at how the socket was imported to a component. 
 export default function ChatId({socket}) {
     const [user, setUser]=useState('')
     const router = useRouter()
@@ -11,15 +10,34 @@ export default function ChatId({socket}) {
     const joinRoom=()=>{
       //join with your username you inputted, only you can join this, unless someone else logs in with that same user input
 
-        socket.emit('join_room', user)
+        socket.emit('join_room', user) //see the "join" event, "join_room" is the name of the join event, your event name might be different.
+        // while user are the argument passed, yours might be different. Since it is a chatting system, you should pass a user identification that is unique to that user.
+
     }
 
-    useEffect(() => {
-      //if no user is connected or no url param for the recipient
-        if(!recipient || !user) return
+    useEffect(() => {      
+        //This is how you emit an event on a component's first render
         joinRoom()    
-    }, [recipient]);
+    }, []);
+    
+    useEffect(() => {
+        //when the backet emits an event - "init" event.This is how you listen to it .
+      console.log("something happened")
+      
+      socket.on("init", data=>{
+        //anything to do with the data
+      })
+       socket.on("recieve_message", data=>{
+        //anything to do with the data
+      })
+    }, [socket]);
 
+
+    const [messageList,setMessageList]= useState([])
+      const sendMessage= ()=>{
+            socket.emit("send_message", messageData)     //this is the event while sending message, if you notice, they have similar syntax with the "join room"
+            setMessageList(list=> [messageData, ...list])
+        }
     
     return (
       <div>
