6b4cec5e0f1ee1bdfc1c859c80e1e586379aa5cd
Victor Ogbonna <victorgbonna@gmail.com>
2022-11-04 12:05:17 +0100

    Update _app.js

--- frontendSocket/src/pages/_app.js
+++ frontendSocket/src/pages/_app.js
@@ -1,15 +1,35 @@
+//this is the _app.js file in next js, there is a file similar in react. App.js file is similar
 import { useState, useEffect } from "react";
 import io from "socket.io-client"
-
-const socket= io.connect('http://localhost:3001')
+//npm install socket.io-client
+const socket= io.connect('http://localhost:3001') //this should be the api url
 
 function MyApp({ Component, pageProps}) {
-  // const [loading, setLoading] = useState(false);
+
   return (
     <>
-      <Component {...pageProps} socket={socket}/>
+      <Component {...pageProps} socket={socket}/> //look at how I passed the socket
     </>
   );
 }
 
+
 export default MyApp;
+
+//react App.js should be like this
+//import "./App.css";
+//import { BrowserRouter, Route,Routes } from "react-router-dom";
+
+//import io from "socket.io-client"
+//const socket= io.connect('http://localhost:3001') //this should be the api url
+
+//function App() {
+  //return (
+  //<Routes>
+    //<Route path='/chat' element={<Chat />} socket={socket}>
+  //</Routes>
+
+  //);
+//}
+
+//export default App;
